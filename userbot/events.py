0543feddc43506db2aae897fdacae32e88f8009a
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-12-19 10:25:20 +0100

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -182,8 +182,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ MAN-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/SharingUserbot)"
+                    text = "**✘ HD-USERBOT ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/hdiiofficialsupport)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
